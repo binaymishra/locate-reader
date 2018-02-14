e0068b321de3096868ed6eb01b3a21b7f058ae37
binaymishramca@gmail.com <binaymishramca@gmail.com>
2018-02-14 07:49:30 +0530

    Initial commit

---
+++ src/main/java/com/locate/reader/Application.java
@@ -0,0 +1,24 @@
+package com.locate.reader;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessagingException;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageHandler;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("imap-idle-mail.xml");
+		DirectChannel inputChannel = context.getBean("inputChannel", DirectChannel.class);
+		inputChannel.subscribe(new MessageHandler() {
+			public void handleMessage(Message<?> message) throws MessagingException {
+				System.out.println("Message: " + message);
+			}
+		});
+
+	}
+
+}
